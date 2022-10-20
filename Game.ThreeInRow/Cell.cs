a9d2e4dea0b22c1c9e15502496642420d832dcb0
Yaroslav-B <91521756+Yaroslav-B@users.noreply.github.com>
2022-10-20 10:27:52 +0300

    feat: add methods to build and print game field

---
+++ Game.ThreeInRow/Cell.cs
@@ -0,0 +1,15 @@
+﻿namespace Game.ThreeInRow
+{
+    class Cell
+    {
+        public int ColumnIndex { get; set; }
+        public int RowIndex { get; set; }
+        public int? Value { get; set; }
+
+        public Cell(int columnIndex, int rowIndex)
+        {
+            ColumnIndex = columnIndex;
+            RowIndex = rowIndex;
+        }
+    }
+}
--- Game.ThreeInRow/Game.cs
+++ Game.ThreeInRow/Game.cs
@@ -14,6 +14,7 @@ namespace Game.ThreeInRow
         private readonly int _maxValue;
         private readonly int _matches;
         private readonly Ilogger _logger;
+        private readonly List<List<Cell>> _gameFiled;
 
         public Game(int rows, int columns, int minValue, int maxValue, int matches, Ilogger logger)
         {
@@ -23,13 +24,72 @@ namespace Game.ThreeInRow
             _maxValue = maxValue;
             _matches = matches;
             _logger = logger;
+
+            _gameFiled = BuildEmptyGameField();
         }
 
         public Game() : this(9, 9, 0, 3, 3, new ConsoleLogger()) { }
 
         public void Start()
         {
+            PopulateEmptyCells();
+            PrintGameField();
+        }
 
+        private List<List<Cell>> BuildEmptyGameField()
+        {
+            var columns = new List<List<Cell>>(_columns);
+
+            for (int columnindex = 0; columnindex < _columns; columnindex++)
+            {
+                var column = new List<Cell>(_rows);
+
+                for (int rowIndex = 0; rowIndex < _rows; rowIndex++)
+                {
+                    var cell = new Cell(columnindex, rowIndex);
+                    column.Add(cell);
+                }
+
+                columns.Add(column);
+            }
+
+            return columns;
+        }
+
+        private void PopulateEmptyCells()
+        {
+            //int c = 0;
+            var r = new Random();
+
+            for (int columnIndex = 0; columnIndex < _gameFiled.Count; columnIndex++)
+            {
+                var column = _gameFiled[columnIndex];
+
+                for (int cellIndex = 0; cellIndex < column.Count; cellIndex++)
+                {
+                    //column[cellIndex].Value ??= c++;
+                    column[cellIndex].Value ??= r.Next(_minValue, _maxValue + 1);
+                }
+            }
+        }
+
+        private void PrintGameField()
+        {
+            int maxFieldCapacity = _columns * _rows + 50;
+            var sb = new StringBuilder(maxFieldCapacity);
+            string space = " ";
+
+            for (int rowIndex = _rows - 1; rowIndex >= 0; rowIndex--)
+            {
+                for (int columnindex = 0; columnindex < _columns; columnindex++)
+                {
+                    sb.Append(_gameFiled[columnindex][rowIndex].Value).Append(space);
+                }
+
+                sb.AppendLine();
+            }
+
+            _logger.Log(sb.ToString());
         }
     }
 }
